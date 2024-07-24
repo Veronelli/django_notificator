de334ce3e4b7363b22071f5bc5818eb5724e4462
Veronelli <facu2000veronelli@outlook.com>
2024-07-23 23:42:45 -0300

    implement queues

--- newapp/tasks.py
+++ newapp/tasks.py
@@ -1,9 +1,22 @@
 from celery import shared_task
+import time
 
 @shared_task
-def task1():
+def task1(queue='celery'):
+    time.sleep(3)
     return
 
 @shared_task
-def task2():
+def task2(queue='celery:1'):
+    time.sleep(3)
+    return
+
+@shared_task
+def task2(queue='celery:2'):
+    time.sleep(3)
+    return
+
+@shared_task
+def task3(queue='celery:3'):
+    time.sleep(3)
     return
